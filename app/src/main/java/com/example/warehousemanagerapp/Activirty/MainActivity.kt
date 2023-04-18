3372b5679f53e496b9b64dad904e117efdeb8ddc
Lê Đạt <70122193+ledat220497@users.noreply.github.com>
2023-04-18 09:04:53 +0700

    commit app github

---
+++ app/src/main/java/com/example/warehousemanagerapp/Activirty/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.warehousemanagerapp.Activirty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.warehousemanagerapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
